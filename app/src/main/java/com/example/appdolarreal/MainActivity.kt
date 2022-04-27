51e132063ce48e50c56f191028fc12f8cbd17402
Luiz Fernando Rodrigues <lyiz.feh12rodrigues@gmail.com>
2022-04-27 16:57:00 -0300

    finalizando splash screen

--- app/src/main/java/com/example/appdolarreal/MainActivity.kt
+++ app/src/main/java/com/example/appdolarreal/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.tela_inicial)
 
 
         precodolar = findViewById<EditText>(R.id.precodolar)
